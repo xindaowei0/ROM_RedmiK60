a239e3b52f028685069c77e558bc60557edd6aeb
辛道韦 <xindaowei@qq.com>
2024-06-04 03:09:52 +0800

    h

--- admin/assets/js/utilities.js
+++
@@ -1,1 +0,0 @@
-jQuery(function(e){e(".jw-pagefactory-media-toolbar").length>0&&e(window).on("scroll",function(){e(window).scrollTop()>220?e(".jw-pagefactory-media-toolbar").addClass("fixed"):e(".jw-pagefactory-media-toolbar").removeClass("fixed")})});
